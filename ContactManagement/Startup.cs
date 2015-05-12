1355b831b49e50ce4baa5635b9639f0af56d85ba
Abbas Hamza <abbas2.hamza@live.uwe.ac.uk>
2015-05-12 12:51:55 +0100

    test

---
+++ ContactManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactManagement.Startup))]
+namespace ContactManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
